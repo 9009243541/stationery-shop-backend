03c75d42d3c60e6a7521106e7c59a8d31f7c4eb5
samyak jain <142584719+9009243541@users.noreply.github.com>
2025-08-20 17:30:08 +0530

    remove authentication from cart

--- api/src/cart/route.js
+++ api/src/cart/route.js
@@ -3,18 +3,18 @@ const cartController = require("./controller");
 const authenticate = require("../middleware/authToken");
 
 // Add product to cart
-router.post("/add", authenticate(), cartController.addToCart);
+router.post("/add", cartController.addToCart);
 
 // Remove product from cart
-router.post("/remove", authenticate(), cartController.removeFromCart);
+router.post("/remove", cartController.removeFromCart);
 
 // Update product quantity in cart
 router.post("/update-quantity", authenticate(), cartController.updateQuantity);
 
 // Get cart for a user
-router.get("/get", authenticate(), cartController.getCart);
+router.get("/get", cartController.getCart);
 
 // Delete entire cart for a user
-router.delete("/delete", authenticate(), cartController.deleteCart);
+router.delete("/delete", cartController.deleteCart);
 
 module.exports = router;
